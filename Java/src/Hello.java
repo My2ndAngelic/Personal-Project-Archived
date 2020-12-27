1d38bcf0385ab88db9fcb1206270fb69e368cc55
My2ndAngelic <8387105+My2ndAngelic@users.noreply.github.com>
2020-12-27 01:27:13 +0100

    Update Hello.java

--- Java/src/Hello.java
+++ Java/src/Hello.java
@@ -7,5 +7,4 @@ public class Hello {
         double[][] b = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}}; // 3 x 4 matrix
         System.out.println(Arrays.deepToString(MatrixMultiplication.matrixMultiplication(a, b)));
     }
-
 }
