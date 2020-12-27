edc3dd34d46fbf57ac33feac5503f6c4714c15f3
My2ndAngelic <8387105+My2ndAngelic@users.noreply.github.com>
2020-12-27 03:00:40 +0100

    AAA

--- Java/src/Hello.java
+++ Java/src/Hello.java
@@ -1,10 +1,15 @@
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
         double[][] a = {{1, 2, 3}}; // 2 x 3 matrix
         double[][] b = {{4},{5},{6}}; // 3 x 4 matrix
-        System.out.println(Arrays.deepToString(MatrixMultiplication.matrixMultiplication(a, b)));
+        System.out.println(Arrays.deepToString(LinearAlgebraUtils.matrixMultiplication(a, b)));
+        double[] c = {1, 2, 3};
+        double[] d = {4, 5, 6};
+        System.out.println(LinearAlgebraUtils.vectorDotProduct(c, d));
+        double[][] e = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+        System.out.println(LinearAlgebraUtils.isSquareMatrix(e));
+        System.out.println(LinearAlgebraUtils.determinant(e));
     }
 }
---
+++ Java/src/LinearAlgebraUtils.java
@@ -0,0 +1,126 @@
+import java.util.Arrays;
+
+public class LinearAlgebraUtils {
+    public static double[][] matrixMultiplication(double[][] a, double[][] b) {
+        double[][] c = new double[a.length][b[0].length];
+        if (!matrixCheck(a) || !matrixCheck(b)) {
+            throw new ArithmeticException("Your input is not a matrix.");
+        }
+        if (!dimensionAgreement(a, b)) {
+            throw new ArithmeticException("Your matrices do not agree in dimension.");
+        }
+        for (int i = 0; i < c.length; i++) {
+            for (int j = 0; j < c[0].length; j++) {
+                double[] temp1 = rowExtractor(a, i);
+                double[] temp2 = columnExtractor(b, j);
+                double cij = 0;
+                for (int k = 0; k < temp1.length; k++) {
+                    cij += temp1[k] * temp2[k];
+                }
+                c[i][j] = cij;
+            }
+        }
+        return c;
+    }
+
+    public static boolean matrixCheck(double[][] a) {
+        int temp = a[0].length;
+        for (int i = 1; i < a.length; i++) {
+            if (a[i].length != temp) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static double[] rowExtractor(double[][] input, int no) {
+        double[] output = new double[input[no].length];
+        for (int i = 0; i < input[no].length; i++) {
+            output[i] = input[no][i];
+        }
+        return output;
+    }
+
+    public static double[] columnExtractor(double[][] input, int no) {
+        double[] output = new double[input.length];
+        for (int i = 0; i < input.length; i++) {
+            output[i] = input[i][no];
+        }
+        return output;
+    }
+
+    public static boolean dimensionAgreement(double[][] in1, double[][] in2) {
+        int a = in1[0].length;
+        for (double[] ints : in1) {
+            if (ints.length != a) {
+                return false;
+            }
+        }
+        return in2.length == a;
+    }
+
+    public static double vectorDotProduct(double[] a, double[] b) {
+        double out = 0;
+        if (a.length != b.length) {
+            throw new ArithmeticException("Input vectors are not in the same space.");
+        }
+        for (int i = 0; i < a.length; i++) {
+            out += a[i] * b[i];
+        }
+        return out;
+    }
+
+    public static boolean isSquareMatrix(double[][] a) {
+        if (!matrixCheck(a)) {
+            throw new ArithmeticException("Check your matrix.");
+        }
+        return rowLength(a) == columnLength(a);
+    }
+
+    public static int rowLength(double[][] a) {
+        return rowExtractor(a,0).length;
+    }
+
+    public static int columnLength(double[][] a) {
+        return columnExtractor(a,0).length;
+    }
+
+    // Start SOF part: https://stackoverflow.com/questions/16602350/calculating-matrix-determinant
+    private static double determinant1(double[][] a, int n) {
+        double res;
+        if (n == 1) {
+            res = a[0][0];
+        } else if (n == 2) {
+            res = a[0][0] * a[1][1] - a[1][0] * a[0][1];
+        } else {
+            res = 0;
+            for (int j1=0; j1< n; j1++){
+                double[][] m = generateSubArray (a, n, j1);
+                res += Math.pow(-1.0, 1.0+j1+1.0) * a[0][j1] * determinant1(m, n-1);
+            }
+        }
+        return res;
+    }
+
+    public static double[][] generateSubArray (double A[][], int N, int j1){
+        double[][] m = new double[N-1][];
+        for (int k=0; k<(N-1); k++)
+            m[k] = new double[N-1];
+
+        for (int i=1; i<N; i++){
+            int j2=0;
+            for (int j=0; j<N; j++){
+                if(j == j1)
+                    continue;
+                m[i-1][j2] = A[i][j];
+                j2++;
+            }
+        }
+        return m;
+    }
+
+    public static double determinant(double[][] a) {
+        return determinant1(a, a.length);
+    }
+    // End SOF part
+}
