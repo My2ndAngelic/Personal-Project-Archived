1729838e92de5090acbe5e974a29baaed53c3579
My2ndAngelic <8387105+My2ndAngelic@users.noreply.github.com>
2020-12-27 22:34:43 +0100

    Upgrade something

---
+++ C-Sharp/C-Sharp/MathUtils/LinearAlgebraUtils.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CSharp.MathUtils
+{
+    public class LinearAlgebraUtils
+    {
+        public static double[] RowExtractor(double[,] a, int rowNo)
+        {
+            var c = new double[a.GetLength(0)];
+            for (int i = 0; i < c.Length; i++)
+            {
+                c[i] = a[i, rowNo];
+            }
+            return c;
+        }
+    }
+}
--- C-Sharp/C-Sharp/Program.cs
+++ C-Sharp/C-Sharp/Program.cs
@@ -6,13 +6,22 @@ namespace CSharp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World Main");
-			Console.WriteLine(Basic.GCD(56, 7));
-			Console.WriteLine(Basic.SF1947.LeMain(679));
-            Console.WriteLine(Basic.Palindrome("dogma i am god"));
-            School a = new Student("John",15,"SF","High");
-            Console.WriteLine(a.ToString());
-            Test();
+   //          Console.WriteLine("Hello World Main");
+			// Console.WriteLine(Basic.GCD(56, 7));
+			// Console.WriteLine(Basic.SF1947.LeMain(679));
+   //          Console.WriteLine(Basic.Palindrome("dogma i am god"));
+   //          School a = new Student("John",15,"SF","High");
+   //          Console.WriteLine(a.ToString());
+   //          Test();
+
+            double[,] aa1 = {{1, 2}, {3, 4}};
+            double[][] aa2 =
+            {
+	            new double[] {1, 2},
+	            new double[] {3, 4}
+            };
+            Console.WriteLine(aa1.Length);
+            Console.WriteLine(aa2[0].Length);
         }
 
         public static void Test()
