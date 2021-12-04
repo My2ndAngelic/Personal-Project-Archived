1faa9ad947a8d0ff6ac97fdaff35325f54bd0902
My2ndAngelic <8387105+My2ndAngelic@users.noreply.github.com>
2021-12-04 03:39:17 +0100

    Update AoCMain.java

--- Java/src/AoC2021/AoCMain.java
+++ Java/src/AoC2021/AoCMain.java
@@ -1,18 +1,24 @@
 package AoC2021;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class AoCMain {
     public static void main(String[] args) throws Exception {
         Scanner userInput = new Scanner(System.in);
-        System.out.print("Enter the path to folder: ");
-        String folder = userInput.nextLine();
-        for (int i = 1; i <= 5; i ++) {
-            ArrayList<Integer> al = AoCUtilities.fileImportToIntegerArrayList(folder+"\\input"+i+".txt");
-            System.out.println("Day 1: " + Package.getPackages("AoC2021").getClass().problemOne(al) + " " + AoC1.problemTwo(al));
-        }
-
+//        System.out.print("Enter the path to folder: ");
+//        String folder = userInput.nextLine();
+//        for (int i = 1; i <= 5; i ++) {
+//            ArrayList<Integer> al = AoCUtilities.fileImportToIntegerArrayList(folder+"\\input"+i+".txt");
+//       }
+        String folder = "C:\\Users\\My2ndAngelic\\Documents\\GitHub\\Personal-Project\\Java\\src\\AoC2021\\input";
+        int i = 1;
+        Class<?> c = Class.forName("AoC2021.AoC1");
+        ArrayList<Integer> al = AoCUtilities.fileImportToIntegerArrayList(folder+"\\input"+i+".txt");
+        Method m = c.getDeclaredMethod("problemOne",ArrayList.class);
+        System.out.println(m.invoke(ArrayList.class, al));
     }
 }
